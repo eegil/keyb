ebc905a3f0f1ea0b7b361bea4fae1b6a1da7b0a6
eegil <bobeidinu@gmail.com>
2022-12-04 11:58:52 +0200

    q
    e

---
+++ sofle/keymaps/d6-2/rules.mk
@@ -0,0 +1,17 @@
+
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+ENCODER_ENABLE = yes
+CONSOLE_ENABLE = no         # Console for debug
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+VIA_ENABLE = yes
+LTO_ENABLE = yes
+TAP_DANCE_ENABLE = yes
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+SPLIT_KEYBOARD = yes
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # Enable N-Key Rollover
+AUDIO_ENABLE = no           # Audio output
